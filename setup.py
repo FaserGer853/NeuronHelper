2841d13d6629a24049d986504d3502effe530124
FaserGer853 <90092906+FaserGer853@users.noreply.github.com>
2023-05-29 11:01:48 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='NeutronHelper',
+      version='1.0',
+      description='AI Simple python helper',
+      author='Joooai',
+      author_email='funnycartoonsyt@gmail.com',
+      url='https://github.com/FaserGer853/NeuronHelper/blob/main/NeuronHelper.py',
+      packages=['NeutronHelper'],
+     )
